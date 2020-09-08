fa64d610bdd81c8565f232bbb822f6f303a16a9f
German <gd.martinez@uniandes.edu.co>
2020-09-08 17:12:08 -0500

    Update script

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Con fetch
+
 const fetchProductList =
   "https://gist.githubusercontent.com/josejbocanegra/be0461060d1c2d899740b8247089ba22/raw/916d2141e32e04031bda79c8886e8e4df0ae7f24/productos.json";
 const fetchDetailList =
@@ -12,6 +14,73 @@ fetch(fetchProductList)
       .then((responseDetailList) => {
         return responseDetailList.json();
       })
+      .then((productDetail) => {
+        // Número de productos en la lista de productos
+        let productsSize = productList.length;
+
+        // Array para almacenar la cantidad de productos que se han vendido por cada producto y se inicializa en cero todas las posiciones
+        let arrayOfProducts = new Array(productsSize).fill(0);
+
+        // Recorrer detalle de productos y sumar la cantidad vendida
+        productDetail.forEach(
+          (item) =>
+            (arrayOfProducts[parseInt(item.idproducto) - 1] += parseInt(
+              item.cantidad
+            ))
+        );
+
+        // Producto con más ventas
+        let max = 0;
+
+        // Posición del producto con más ventas. Iniciado en -1.
+        let position = -1;
+
+        // Recorrer array para encontrar el mayor
+        for (let index = 0; index < arrayOfProducts.length; index++) {
+          if (arrayOfProducts[index] > max) {
+            max = arrayOfProducts[index];
+            position = index;
+          }
+        }
+
+        // Respuesta
+        console.log(
+          "El producto más pedido es: " +
+            productList[position].nombreProducto +
+            ". Ha sido pedido " +
+            max +
+            " veces."
+        );
+      });
+  });
+
+// Sin fetch
+
+const request = (url) => {
+  return new Promise((resolve, reject) => {
+    let req = new XMLHttpRequest();
+    req.responseType = "json";
+    req.open("GET", url);
+    req.onload = function () {
+      if (req.status === 200) {
+        resolve(req.response);
+      } else reject(req.status);
+    };
+    req.send();
+  });
+};
+
+const productList2 = request(
+  "https://gist.githubusercontent.com/josejbocanegra/be0461060d1c2d899740b8247089ba22/raw/916d2141e32e04031bda79c8886e8e4df0ae7f24/productos.json"
+);
+
+productList2
+  .then((productList) => {
+    const detailList2 = request(
+      "https://gist.githubusercontent.com/josejbocanegra/7b6febf87e9d986048a648487b35e693/raw/576531a2d0e601838fc3de997e021816a4b730f8/detallePedido.json"
+    );
+
+    detailList2
       .then((productDetail) => {
         // Número de productos en la lista de productos
         let productsSize = productList.length;
